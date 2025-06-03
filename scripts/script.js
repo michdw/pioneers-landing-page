ece9d585482d04a6e091e820aa39bdd1eec6b6fc
michdw <michd.weaver@gmail.com>
2025-06-03 15:51:13 -0400

    wrapped window.scroll inside window.load

--- scripts/script.js
+++ scripts/script.js
@@ -15,5 +15,4 @@ $(document).ready(function () {
 
 $(window).load(function() {
     $('#loading-screen').hide();
-
 })
--- scripts/transitions.js
+++ scripts/transitions.js
@@ -3,54 +3,57 @@ let $window = $(window),
   $body = $('body'),
   $panel = $('.panel');
 
-$window.scroll(function () {
+$window.load(function () {
+  $window.scroll(function () {
 
-  // navbar and image behavior dependent on scroll position
-  if ($window.scrollTop() > 0) $('#landing-img').fadeOut(200);
+    // navbar and image behavior dependent on scroll position
+    if ($window.scrollTop() > 0) $('#landing-img').fadeOut(200);
 
-  if ($window.scrollTop() > $window.height() / 10) {
-    $('h1').css('opacity', '0');
-    $('nav').removeClass('nav-full').addClass('nav-diminished');
-  }
-  if ($window.scrollTop() == 0) {
-    $('h1').css('opacity', '1')
-    $('#landing-img').delay(100).fadeIn(200);
-    $('nav').removeClass('nav-diminished').addClass('nav-full');
-  }
-
-  // define the point at which bg color changes between panels
-  let scroll = $window.scrollTop() + ($window.height() / 2);
-
-  // show lighter text on navbar link for current location
-  function updateNavLinks(section) {
-    if (($body).hasClass('at-' + section)) {
-      let $activeNavLink = $('a[href="#' + section + '"]')
-      $activeNavLink.addClass('active-navlink');
-      $('a').not($activeNavLink).removeClass('active-navlink');
+    if ($window.scrollTop() > $window.height() / 10) {
+      $('h1').css('opacity', '0');
+      $('nav').removeClass('nav-full').addClass('nav-diminished');
     }
-  }
-
-  $panel.each(function () {
-    let $this = $(this);
-    
-    // color transitions
-    if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
-
-      $body.removeClass(function (index, css) {
-        return (css.match(/(^|\s)at-\S+/g) || []).join(' ');
-      });
-
-      let atSection = $(this).data('at');
-
-      $body.addClass('at-' + atSection);
-
-      // nav link transitions
-      updateNavLinks('landing')
-      // updateNavLinks('info')
-      updateNavLinks('give')
-      updateNavLinks('subscribe')
-
+    if ($window.scrollTop() == 0) {
+      $('h1').css('opacity', '1')
+      $('#landing-img').delay(100).fadeIn(200);
+      $('nav').removeClass('nav-diminished').addClass('nav-full');
     }
-  });
 
-}).scroll();
+    // define the point at which bg color changes between panels
+    let scroll = $window.scrollTop() + ($window.height() / 2);
+
+    // show lighter text on navbar link for current location
+    function updateNavLinks(section) {
+      if (($body).hasClass('at-' + section)) {
+        let $activeNavLink = $('a[href="#' + section + '"]')
+        $activeNavLink.addClass('active-navlink');
+        $('a').not($activeNavLink).removeClass('active-navlink');
+      }
+    }
+
+    $panel.each(function () {
+      let $this = $(this);
+
+      // color transitions
+      if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
+
+        $body.removeClass(function (index, css) {
+          return (css.match(/(^|\s)at-\S+/g) || []).join(' ');
+        });
+
+        let atSection = $(this).data('at');
+
+        $body.addClass('at-' + atSection);
+
+        // nav link transitions
+        updateNavLinks('landing')
+        // updateNavLinks('info')
+        updateNavLinks('give')
+        updateNavLinks('subscribe')
+
+      }
+    });
+
+  }).scroll();
+})
+
