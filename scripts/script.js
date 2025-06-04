c5f0f78857b324fc9116f3dda3dbe144186f1742
michdw <michd.weaver@gmail.com>
2025-06-03 23:29:55 -0400

    formatting

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,12 @@
+// let animateScroll = true;
+
 $(document).ready(function () {
     $('#mailform').load('components/mailform.html')
+    $('#down-arrow').click(function() {
+        $('html,body').animate({
+            scrollTop: $('#info').offset().top
+        }, 150);
+    })    
 });
 
 $(window).load(function() {
--- scripts/transitions.js
+++ scripts/transitions.js
@@ -1,53 +1,53 @@
 // selectors
 let $window = $(window),
   $body = $('body'),
-  $panel = $('.panel');
+  $panel = $('.panel')
 
 $window.load(function () {
   $window.scroll(function () {
 
     // navbar and image behavior dependent on scroll position
-    if ($window.scrollTop() > 0) $('#landing-img').fadeOut(200);
+    if ($window.scrollTop() > 0) $('#landing-img').fadeOut(200)
 
     if ($window.scrollTop() > $window.height() / 10) {
-      $('#name-heading').css('opacity', '0');
-      $('nav').removeClass('nav-full').addClass('nav-diminished');
+      $('#name-heading').css('opacity', '0')
+      $('nav').removeClass('nav-full').addClass('nav-diminished')
     }
     if ($window.scrollTop() == 0) {
       $('#name-heading').css('opacity', '1')
-      $('#landing-img').delay(200).fadeIn(150);
-      $('nav').removeClass('nav-diminished').addClass('nav-full');
+      $('#landing-img').delay(200).fadeIn(150)
+      $('nav').removeClass('nav-diminished').addClass('nav-full')
     }
 
     // define the point at which bg color changes between panels
-    let scroll = $window.scrollTop() + ($window.height() / 2);
+    let scroll = $window.scrollTop() + ($window.height() / 2)
 
     // show lighter text on navbar link for current location
     function updateNavLinks(section) {
       if (($body).hasClass('at-' + section)) {
         let $activeNavLink = $('a[href="#' + section + '"]')
-        $activeNavLink.addClass('active-navlink');
-        $('a').not($activeNavLink).removeClass('active-navlink');
+        $activeNavLink.addClass('active-navlink')
+        $('a').not($activeNavLink).removeClass('active-navlink')
       }
     }
 
     $panel.each(function () {
-      let $this = $(this);
+      let $this = $(this)
 
       // color transitions
       if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
 
         $body.removeClass(function (index, css) {
-          return (css.match(/(^|\s)at-\S+/g) || []).join(' ');
-        });
+          return (css.match(/(^|\s)at-\S+/g) || []).join(' ')
+        })
 
-        let atSection = $(this).data('at');
+        let atSection = $(this).data('at')
 
-        $body.addClass('at-' + atSection);
+        $body.addClass('at-' + atSection)
 
         // nav link transitions
         updateNavLinks('landing')
-        // updateNavLinks('info')
+        updateNavLinks('info')
         updateNavLinks('give')
         updateNavLinks('subscribe')
 
