9e5e1563c290605a18836134af2717d215170d12
michdw <michd.weaver@gmail.com>
2025-06-09 18:11:06 -0400

    integrated mail form with index.html

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,5 @@
-// $('#down-arrow').click(function () {
-//     $('html,body').animate({
-//         scrollTop: $('#info').offset().top
-//     }, 150);
-// })
-// $('#red-arrow').click(function () {
-//     $('html,body').animate({
-//         scrollTop: $('#give').offset().top
-//     }, 150);
-// })
-
-
-
 $(document).ready(function () {
-    $('#mailform').load('components/mailform.html')
+    // $('#mailform').load('components/mailform.html')
     $('.dbl-arrow').click(function () {
         let dest = '#' + $(this).data('goto')
         $('html,body').animate({
